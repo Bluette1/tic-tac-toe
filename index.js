22644dd6bf620ddc9103d31728450b14be766088
Bluette1 <marylene.sawyer@gmail.com>
2020-10-02 17:00:34 +0200

    Commit latest changes - logic phase

--- index.js
+++ index.js
@@ -90,8 +90,8 @@ const Board = () => {
 const Game = () => {
     const board = Board();
     let turnCounter = 0;
-    const firstPlayer = null;
-    const secondPlayer = null;
+    let firstPlayer = null;
+    let secondPlayer = null;
 
 
     const players = (firstPlayer, secondPlayer) => {
@@ -150,7 +150,60 @@ const Game = () => {
 
         return true;
     };
-
-
     return { turnCounter };
 };
+
+console.log('Welcome to Tic Tac Toe!');
+// loop do# rubocop: todo Metrics / BlockLength
+let gameOn = true;
+while (gameOn) {
+    console.log("Would you like to play a game of tic tac toe? Please enter ' [y] es ' or ' [n] o '");
+}
+// puts "Would you like to play a game of tic tac toe? Please enter '[y]es' or '[n]o'"
+// play_game = gets.chomp[0].downcase
+// let playGame =
+// while % w[y n].include ? (play_game) == false
+// puts "Please enter '[y]es' or '[n]o'"
+// play_game = gets.chomp.downcase
+// end
+// if play_game[0] == 'y'
+// puts 'We are going to start the game shortly after asking you a few more questions'
+// game = Game.new
+// puts 'What is the name of first player'
+// first_player = gets.chomp.capitalize
+// puts 'What is the name of second player'
+// second_player = gets.chomp.capitalize
+// game.players(first_player, second_player)
+// puts game.print_board
+// puts game.player_info
+// puts 'Do you want to change the symbols of players. If yes, push [y]'
+// chosen = gets.chomp.downcase
+// game.change_symbol(first_player, second_player) if chosen[0] == 'y'
+// puts game.player_info
+// loop do
+//     game.check_player
+// puts "It is #{game.check_player.keys[0]}'s turn"
+// loop do
+//     puts game.print_board
+// puts 'Choose the number to play'
+// chosen = gets.chomp.to_i
+// break unless game.verify_update(chosen).is_a ? String
+
+// puts game.verify_update(chosen)
+// end
+// winner = game.check_win
+// if winner and game.turn_counter >= 4
+// puts "There is a WINNER: #{game.check_player.keys[0]}"
+// break
+// end
+// game.turn_increase
+// next unless game.turn_counter == 9
+
+// puts 'DRAW'
+// break
+// end
+// elsif play_game[0] == 'n'
+// puts 'Hope to see you again. Bye.'
+// break
+// end
+// end
