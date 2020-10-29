06c373af98d32b3fdbbbe787e2d1731da9886e61
Bluette1 <marylene.sawyer@gmail.com>
2020-10-29 05:04:59 +0200

    Add test for GameBoard class

--- test/gameBoard.test.js
+++ test/gameBoard.test.js
@@ -2,8 +2,25 @@
  * @jest-environment jsdom
  */
 
-// import GameFlow from '../src/gameBoard';
-
-test('scenario', () => {
+import Board from '../src/gameBoard';
+import {bodyContent, initialBoard} from './helpers/content_test_helper';
   // Inject DOM element, and add code to test logic
+document.body.innerHTML = bodyContent;
+const introHeader = document.querySelector('#intro-header');
+const optionsDiv = document.querySelector('#options-div');
+const namesForm = document.querySelector('#names-form');
+const gameBoardSection = document.querySelector('#gamebord-section');
+const newRoundBtn = document.querySelector('#new-round-btn');
+const gameBoardContainer = document.querySelector('#gamebord-container');
+
+test('displayBoard adds content to the game board container', () => {
+
+optionsDiv.classList.add('hidden-element');
+        namesForm.classList.add('hidden-element');
+        introHeader.classList.add('hidden-element');
+        gameBoardSection.classList.remove('hidden-element');
+        gameBoardContainer.setAttribute('disabled', false);
+        Board(['Mary', 'Jane'], introHeader, optionsDiv, newRoundBtn).displayBoard();
+        expect(gameBoardContainer.innerHTML).not.toBe('');
+        expect(gameBoardContainer.innerHTML).toBe(initialBoard);
 });
