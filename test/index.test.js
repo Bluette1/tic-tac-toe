fe9ed57fa9041ede3618a38824b10e1156a5a8e0
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-30 16:23:52 +0200

    remove unnecessary file

--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-/**
- * @jest-environment jsdom
- */
-// import * as Index from '../src/index';
-// import { bodyContent, initialBoard } from './helpers/content_test_helper';
-// Inject DOM element, and add code to test logic
-test('scenario', () => {});
